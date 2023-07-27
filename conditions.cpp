b5f6c89a61bc5748adf36b2546aaa3d5784a0011
Shivam <thisisshivam06@gmail.com>
2023-07-27 23:49:48 +0530

    Conditions & Loops

---
+++ conditions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+ 
+//  cout<<" value of n is :" << n <<endl;
+
+//if a is positive
+if(a>0){
+    cout<<" A is Positive" << endl;
+}else{
+    cout<<" A is negative" << endl;
+}
+}
