917b5416bf083ac7a48ed8278b940cc48ec20000
Shivam <thisisshivam06@gmail.com>
2023-08-08 23:37:28 +0530

    Final Commit

--- conditions.cpp
+++ conditions.cpp
@@ -2,15 +2,57 @@
 using namespace std;
 
 int main() {
-    int a;
-    cin>>a;
+    // int a;
+    // cin>>a;
  
 //  cout<<" value of n is :" << n <<endl;
 
 //if a is positive
-if(a>0){
-    cout<<" A is Positive" << endl;
-}else{
-    cout<<" A is negative" << endl;
+// if(a>0){
+//     cout<<" A is Positive" << endl;
+// }else{
+//     cout<<" A is negative" << endl;
+// }
+
+// int a,b;
+
+// cin>> a >> b;
+
+// a = cin.get();
+
+
+// cout<<" value of a and b is "<<a <<" "<< b <<endl;
+
+// cout<<" value of a is : "<< a << endl; 
+
+// int a,b;
+
+// cout<<"Enter the value of a "<<endl;
+// cin>>a;
+// cout<<"Enter the value of b "<<endl;
+// cin>>b;
+
+// if(a>b){
+//     cout<<" A is greater " << endl;
+// }
+// if(b>a){
+//     cout<<" B is greater " << endl;
+// }
+
+int a ;
+cout<<" enter the value of a "<<endl;
+cin>>a;
+
+if(a>0) {
+    cout<<" A is positive";
 }
+else{
+    if(a<0) {
+       cout<<"A is negative";
+    }
+    else{
+        cout<<" A is 0";
+    }
+}
+
 }
