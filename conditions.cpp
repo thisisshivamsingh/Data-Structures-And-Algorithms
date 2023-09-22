ab5c191a139c735fc3999e67828c3fc8c60f1e90
Shivam <thisisshivam06@gmail.com>
2023-09-22 23:59:41 +0530

    Final

--- conditions.cpp
+++ conditions.cpp
@@ -39,22 +39,65 @@ int main() {
 //     cout<<" B is greater " << endl;
 // }
 
-int a ;
-cout<<" enter the value of a "<<endl;
-cin>>a;
+// int a ;
+// cout<<" enter the value of a "<<endl;
+// cin>>a;
 
-if(a>0) {
-    cout<<" A is positive";
+// if(a>0){
+//     cout<<" A is positive" << endl;
+// }else if(a<0){
+//     cout<<" A is negative"<<endl;
+// }else{
+//     cout<<" A is 0"<<endl;
+// }
+
+// int a = 9;
+// if(a==9){
+//     cout<< "NINEY";
+// }
+
+// if(a > 0){
+//     cout<<"POSITIVE";
+// }
+// else
+// {
+//     cout<<"NEGATIVE";
+// }
+
+// int a = 2;
+// int b = a+1;
+
+// if((a=3)==b){
+//     cout<<a;
+// }
+// else
+// {
+//     cout<<a+1;
+// }
+
+// int a = 24;
+
+// if(a > 20){
+//     cout<< "Love ";
+// }else if(a == 24) {
+//     cout<<"Lovely";
+// }
+// else
+// {
+//     cout<<"Babbar";
+// }
+// cout << a;
+
+char ch;
+
+cin >> ch;
+
+if(97<=ch&&ch<=122){
+    cout << "This is Lowercase small case" << endl;
+}else if(65<=ch&&ch<=90){
+ cout << "This is Upper Case" << endl;
+}else if(48<=ch&&ch<=57){
+cout << "This is numeric" << endl;
 }
-else{
-    if(a<0) {
-       cout<<"A is negative";
-    }
-    else{
-        cout<<" A is 0";
-    }
 }
 
-}
-
-// Hello World! //
