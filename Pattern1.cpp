a6d4e7157d9f8ae50f6fcbd95d3b694cc4e829f7
Shivam <thisisshivam06@gmail.com>
2023-10-04 23:16:55 +0530

    Final Commit

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -92,22 +92,47 @@ int main() {
 //     row = row + 1;
 // }
 
+// int n;
+// cin>>n;
+
+// int row = 1;
+// int count = 1;
+// while(row<=n){
+//     int col = 1;
+// while(col<=row){
+// cout<<count<<" ";
+// count = count + 1;
+// col = col + 1; 
+// }
+// cout<<endl;
+// row = row + 1; 
+// }
+
+// int n;
+// cin>>n;
+
+// int row = 1;
+
+// while(row<=n){
+    
+//     int col = 1;
+ 
+//     while(col <= row){
+//         cout<<col+row-1;
+
+//         col = col + 1;
+//     }
+//     cout<<endl;
+//     row = row + 1;
+
+// }
+
 int n;
 cin>>n;
 
-int row = 1;
-int count = 1;
-while(row<=n){
-    int col = 1;
-while(col<=row){
-cout<<count<<" ";
-count = count + 1;
-col = col + 1; 
-}
-cout<<endl;
-row = row + 1; 
-}
+int i = 1;
 
+while(i <= n){}
 
 }
 
