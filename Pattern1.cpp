d53186fd4637cb6bd7f2de76980ed9dc25fb8f47
Shivam <thisisshivam06@gmail.com>
2023-10-05 01:58:08 +0530

    Pattern

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -127,12 +127,102 @@ int main() {
 
 // }
 
+// int n;
+// cin>>n;
+
+// int i = 1;
+
+// while(i <= n){
+//     int j = 1;
+//     while(j <= i){
+//         cout<<(i-j+1)<<" ";
+//         j = j + 1;
+//     }
+//     cout<<endl;
+//     i = i + 1;
+// }
+
+// int n;
+// cin>>n;
+
+// int row = 1;
+// while(row <= n){
+//     int col = 1;
+//     while(col <= n){
+//         char ch = 'A' + row - 1;
+//         cout<< ch;
+//         col = col + 1;
+//     }
+//     cout<<endl;
+//     row = row + 1;
+// }
+
+// int n;
+// cin>>n;
+
+// int row = 1;
+// while(row<=n){
+//     int col = 1;
+//     while(col<=n){
+//         char ch = 'A' + col - 1;
+// cout<<ch;
+// col = col +1 ;
+
+
+//     }
+//     cout<<endl;
+//     row = row + 1;
+// }
+
+// int n;
+// cin>>n;
+
+// int row = 1;
+// char ch ='A';
+// while(row<=n){
+//     int col = 1;
+
+//     while(col<=n){
+
+// cout<< ch;
+// ch = ch + 1;
+// col = col+1;
+//     }
+//     cout<<endl;
+
+// row = row + 1;
+// }
+
+// int n;
+// cin >> n;
+// int row = 1;
+
+// while(row<=n){
+//     int col = 1;
+//     while(col<=n){
+// char ch = 'A' + row + col -2;
+// cout << ch;
+// col = col + 1;
+//     }
+//     cout<<endl;
+//     row = row + 1;
+// }
+
 int n;
 cin>>n;
 
-int i = 1;
-
-while(i <= n){}
+int row = 1;
+while(row<=n){
+    int col = 1;
+    char ch = 'A'+row-1;
+    while(col<=row){
+cout << ch;
+col = col + 1;
+    }
+    cout<< endl;
+   
+    row = row+1;
+}
 
 }
 
