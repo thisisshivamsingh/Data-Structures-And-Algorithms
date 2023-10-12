9b0250b78160e67c64ea0e0d6a1cc20b117d7f3c
thisisshivamsingh <thisisshivam06@gmail.com>
2023-10-12 10:53:49 +0530

    Some Comments

--- conditions.cpp
+++ conditions.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    // int a;
-    // cin>>a;
- 
-//  cout<<" value of n is :" << n <<endl;
 
+//// FIND POSITIVE NUMBER ////
+// int a;
+// cin>>a;
+//  cout<<" value of n is :" << n <<endl;
 //if a is positive
 // if(a>0){
 //     cout<<" A is Positive" << endl;
@@ -14,24 +14,19 @@ int main() {
 //     cout<<" A is negative" << endl;
 // }
 
+//// FIND THE VALUE OF KEYS ////
 // int a,b;
-
 // cin>> a >> b;
-
 // a = cin.get();
-
-
 // cout<<" value of a and b is "<<a <<" "<< b <<endl;
-
 // cout<<" value of a is : "<< a << endl; 
 
+//// WHICH IS GREATER A OR B ////
 // int a,b;
-
 // cout<<"Enter the value of a "<<endl;
 // cin>>a;
 // cout<<"Enter the value of b "<<endl;
 // cin>>b;
-
 // if(a>b){
 //     cout<<" A is greater " << endl;
 // }
@@ -39,10 +34,10 @@ int main() {
 //     cout<<" B is greater " << endl;
 // }
 
+//// FIND THE POSITIVE, NEGITIVE AND ZERO ////
 // int a ;
 // cout<<" enter the value of a "<<endl;
 // cin>>a;
-
 // if(a>0){
 //     cout<<" A is positive" << endl;
 // }else if(a<0){
@@ -51,11 +46,11 @@ int main() {
 //     cout<<" A is 0"<<endl;
 // }
 
+//// OUTPUT QUESTION ////
 // int a = 9;
 // if(a==9){
 //     cout<< "NINEY";
 // }
-
 // if(a > 0){
 //     cout<<"POSITIVE";
 // }
@@ -64,9 +59,9 @@ int main() {
 //     cout<<"NEGATIVE";
 // }
 
+//// OUTPUT QUESTION ////
 // int a = 2;
 // int b = a+1;
-
 // if((a=3)==b){
 //     cout<<a;
 // }
@@ -75,8 +70,8 @@ int main() {
 //     cout<<a+1;
 // }
 
+//// OUTPUT QUESTION ////
 // int a = 24;
-
 // if(a > 20){
 //     cout<< "Love ";
 // }else if(a == 24) {
@@ -88,10 +83,9 @@ int main() {
 // }
 // cout << a;
 
+//// FIND UPPERCASE, LOWERCASE, NUMERIC ////
 // char ch;
-
 // cin >> ch;
-
 // if(97<=ch&&ch<=122){
 //     cout << "This is Lowercase small case" << endl;
 // }else if(65<=ch&&ch<=90){
