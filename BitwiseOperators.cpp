7b657adacb505003638619c72791cb082f8b5d16
Shivam <thisisshivam06@gmail.com>
2023-10-16 02:11:39 +0530

    Bitwise Operators

---
+++ BitwiseOperators.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+
+    // int a = 4;
+    // int b = 6;
+
+    // cout << " a&b " << (a&b) << endl;
+    // cout<<" a|b " << (a|b) << endl;
+    // cout<<" ~a " << ~a << endl;
+    // cout<<" a^b " << (a^b) << endl;
+
+    // cout<< (17>>1) <<endl;
+    // cout<< (17>>2) <<endl;
+    // cout<< (19<<1) <<endl;
+    // cout<< (21<<2) <<endl;
+
+    // int i = 7;
+
+    // cout<< (++i) <<endl;
+    // // 8
+    // cout<< (i++) <<endl;
+    // // 8 , i = 9
+    // cout<< (i--) <<endl;
+    // // 9 , i = 8
+    // cout<< (--i) <<endl;
+    // // 7 , i = 7
+
+    // int a, b = 1;
+    // a = 10;
+    // if(++a)
+    // cout << b;
+    // else
+    // cout << ++b;
+
+    // int a = 1;
+    // int b = 2;
+
+    // if(a-- > 0 && ++b > 2){
+    //     cout << "Stage1 - Inside If ";
+    // } else{
+    //     cout << "Stage2 - Inside else ";
+    // }
+    // cout << a << " " << b << endl;
+
+    // int number = 3;
+    // cout << (25 * (++number));
+
+    // int a = 1;
+    // int b = a++;
+    // int c = ++a;
+    // cout << b ;
+    // cout << c;
+
+}
--- Pattern1.cpp
+++ Pattern1.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    // int n;
-    // cin >> n;
+    int n;
+    cin >> n;
 
-    // int i = 1;
+    int i = 1;
 
-    // while(i<=n){
-    //     int j = 1;
-    //     while(j<=n){
-    //         cout<<"*";
-    //         j=j+1;
-    //     }
-    //     cout<<endl;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
 
-    //     i = i+1;
-    // }
+        i = i+1;
+    }
 
     // int n;
     // cin >> n;
